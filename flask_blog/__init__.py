956f7fb9f3e301a4d22bb548bdf453539a8fa179
kcachero <kcachero@isimarkets.com>
2020-02-24 20:33:47 +0800

    first commit

---
+++ flask_blog/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY']= 'c615d2300fca14d065e66effda70052f'
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///site.db'
+db= SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager= LoginManager(app)
+login_manager.login_view='login'
+login_manager.login_message_category='info'
+
+
+from flask_blog import routes
---
+++ flask_blog/routes.py
@@ -0,0 +1,110 @@
+from flask import Flask, escape, request, render_template,url_for, flash, redirect, abort
+from flask_blog import app, db, bcrypt
+from flask_blog.forms import RegistrationForm, LoginForm, PostForm
+from flask_blog.models import User,Post
+from flask_login import login_user, current_user, logout_user, login_required
+
+
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    posts= Post.query.all()
+    return render_template( 'home.html', posts =posts)
+
+@app.route('/about')
+def about():
+    return render_template( 'about.html',title='About')
+
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username = form.username.data , email =form.email.data, password = hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Your account has been created! You are now able to login!', 'success')
+        return redirect(url_for('login'))
+
+    return render_template('register.html',title='Register',form=form)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user,remember=form.remember.data)
+            next_page =request.args.get('next')
+            return  redirect (next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email / pass','danger')
+    return render_template('login.html',title='Login',form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/account')
+@login_required
+def account():
+    image_file = url_for('static', filename='profile_pic/' + current_user.image_file)
+    return render_template('account.html',title='Account', image_file= image_file)
+
+
+@app.route('/post/new',methods=['GET','POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data , content=form.content.data, author =current_user)
+        db.session.add(post)
+        db.session.commit()
+
+        flash('Your post has been created', 'sucess')
+        return redirect(url_for('home'))
+    return render_template('create_post.html',title='New Post', form = form, legend='New Post')
+
+@app.route('/post/<post_id>')
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+@app.route('/post/<int:post_id>/update', methods=['GET','POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash ('Your post has beeen updated', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+        
+    return render_template('create_post.html',title='Update Post', form = form, legend='Update Post')
+    
+@app.route('/post/<int:post_id>/delete', methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash ('Your post has beeen deleted', 'success')
+    return redirect(url_for('home'))
